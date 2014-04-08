a82cac583df1c95ad5a90b08110acb9d933e6d7b
Max Yankov <golergka@gmail.com>
2014-04-08 23:30:20 +0400

    Semi-working JSON api; at least it returns questions

--- index.js
+++ index.js
@@ -4,7 +4,9 @@
 var	http		= require("http"),
 	url			= require("url"),
 	nconf		= require("nconf"),
-	Sequelize	= require("sequelize");
+	Sequelize	= require("sequelize"),
+	ursa		= require("ursa"),
+	Q			= require("q");
 
 //
 // Configuration
@@ -34,56 +36,192 @@ nconf.defaults({
 //
 
 Sequelize.db = new Sequelize(
-		nconf.get('sequelize:database'),
-		nconf.get('sequelize:username'),
-		nconf.get('sequelize:password'),
-		nconf.get('sequelize:config')
-	)
-.authenticate()
-.complete(function(err) {
-	if (!!err) {
-		console.log('Unable to connect to the database: ' + err);
-	} else {
-		console.log('Establised database connection.');
-	}
+	nconf.get('sequelize:database'),
+	nconf.get('sequelize:username'),
+	nconf.get('sequelize:password'),
+	nconf.get('sequelize:config')
+);
+Sequelize.db
+	.authenticate()
+	.complete(function(err) {
+		if (!!err) {
+			console.log('Unable to connect to the database: ' + err);
+		} else {
+			console.log('Establised database connection.');
+		}
+	});
+
+// Users
+
+Sequelize.db.user = Sequelize.db.define('User', {
+	vendorIdHash:	Sequelize.STRING,
+	loginMethod:	Sequelize.ENUM('iOSVendorId'),
+	publicKey:		Sequelize.STRING
 });
 
-function getQuestions(callback) {
-	// only temporary yet
-	var questions = [
-		{
-			id: 1,
-			text: "Will 2014 ever end?",
-			date: new Date(2014,12,31)
-		},
-		{
-			id: 2,
-			text: "Will the Yellowstone erupt until 3 May, 2014?",
-			date: new Date(2014,5,3)
+// Questions
+
+Sequelize.db.question = Sequelize.db.define('Question', {
+	text: Sequelize.STRING,
+	date: Sequelize.DATE
+});
+
+// Syncing db
+
+Sequelize.db
+	.sync({ force: true })
+	.complete(function(err) {
+		if (!!err) {
+			console.log('Error while synching schemas: ' + err);
+		} else {
+			console.log('Database schemas synced');
 		}
-	];
-	callback(questions);
-}
+	});
 
 //
 // Starting server
 //
 
-var server = http.createServer(function(req,res) {
-	var u = url.parse(req.url, true);
-	
-	res.writeHead(200, { 'Content-Type': 'application/json' });
-	var result = {};
+function Request(path, params, res) {
+	this.path	= path;
+	this.params = params;
+	this.res	= res;
+}
 
-	if (u.pathname === '/getQuestions') {
-		getQuestions(function (questions) {
-			result.questions = questions;
-			res.end(JSON.stringify(result));
-		});
+Request.prototype.getParameter = function getParameter(paramName) {
+	var result = Q.defer();
+	var param = this.params[paramName];
+	if (!param) {
+		result.reject(new Error(paramName + ' parameter required'));
 	} else {
-		result.error = "Unknown url";
-		res.end(JSON.stringify(result));
+		result.resolve(param);
 	}
+	return result.promise;
+};
+
+Request.prototype.validateSignature = function(publicKey) {
+	var result = Q.defer();
+	var checkParams = [];
+	var signature;
+	var message = "";
+
+	for(var param in this.params) {
+		if (this.params.hasOwnKey(param)) {
+			if (param !== 'sig') {
+				checkParams.push(param);
+			} else {
+				signature = this.params[param];
+			}
+		}
+	}
+
+	if (!signature) {
+		result.reject('No signature found!');
+	} else {
+		checkParams.sort();
+		for(var i = 0; i < checkParams.length; i++) {
+			message += checkParams[i] + '=' + this.params[checkParams[i]];
+		}
+		var ursaPublicKey = ursa.createPublicKey(publicKey, 'utf-8');
+		if (ursaPublicKey.hashAndVerify('sha256',message,signature,'utf-8')) {
+			result.resolve();
+		} else {
+			result.reject("Signature doesn't match");
+		}
+	}
+
+	return result;
+};
+
+Request.prototype.registerUser = function() {
+	return this.getParameter('public_key')
+	.then(function(publicKey) {
+		return this.validateSignature(publicKey)
+		.then(this.getParameter('vendor_id_hash'))
+		.then(function(vendorIdHash) {
+			return Sequelize.db.user.create({
+				vendorIdHash:	vendorIdHash,
+				loginMethod:	'iOSVendorId',
+				publicKey:		publicKey
+			});
+		});
+	});
+};
+
+Request.prototype.loginUser = function() {
+	return this.getParameter('vendor_id_hash')
+	.then(function(vendorIdHash){
+		return Sequelize.db.user.find({
+			where: {
+				loginMethod: 'iOSVendorId',
+				vendorIdHash: vendorIdHash
+			}
+		})
+		.then(function(user){
+			return this.validateSignature(user.publicKey);
+		});
+	});
+};
+
+Request.prototype.validate = function() {
+	return this.getParameter('method')
+	.then(function(method) {
+		if (method === 'register') {
+			return this.registerUser();
+		} else {
+			return this.loginUser();
+		}
+	});
+};
+
+Request.prototype.reply = function(reply, code) {
+	this.res.writeHead(code, { 'Content-Type': 'application/json' });
+	this.res.end(JSON.stringify(reply));
+};
+
+Request.prototype.replyError = function(error) {
+	this.reply(this.res,{ error: error },400);
+};
+
+Request.prototype.process = function() {
+	console.log('processing request: ' + this.path);
+	console.log('parameters:');
+	console.log(this.params);
+	var that = this;
+	(function(){
+		switch(that.path) {
+			case '/public':
+				return that.getParameter('method')
+				.then(function(method) {
+					switch(method) {
+						case 'getQuestions':
+							console.log('loading questions');
+							return Sequelize.db.question.findAll()
+							.then(function(questions) {
+								console.log('loaded questions: ');
+								console.log(questions);
+								that.reply(questions, 200);
+							});
+
+						default:
+							throw 'Unknown method ' + method;
+					}
+				});
+			case '/secure':
+				return that.request.validate()
+				.then(that.getParameter('method'));
+			default:
+				throw 'Unknown path ' + that.path;
+		}
+	})()
+	.fail(this.replyError);
+};
+
+var server = http.createServer(function(req,res) {
+	req.setEncoding('utf8');
+	var parsedUrl = url.parse(req.url, true);
+	var path = parsedUrl.pathname;
+	new Request(path, parsedUrl.query, res).process();
 });
 server.listen(nconf.get('http:port'));
 console.log("Listening on port " + nconf.get('http:port'));
