a00d0611ca8a3827d4308cf4b316f853f6d65872
Max Yankov <golergka@gmail.com>
2014-05-03 15:16:29 +0400

    Added migrations on startup

--- database/storage.js
+++ database/storage.js
@@ -24,7 +24,17 @@ exports.sync = function() {
 			db.sync()
 		])
 		.then(function() {
-			console.log('Database schemas synced');
+			console.log('Applying pending migrations...');
+			var result = Q.defer();
+			var migrator = db.getMigrator({ path: process.cwd() + '/database/migrations' });
+			migrator
+			.migrate({ method: 'up' })
+			.success(result.resolve)
+			.error(result.reject);
+			return result.promise;
+		})
+		.then(function() {
+			console.log('Database schemas synced!');
 		});
 
 };
